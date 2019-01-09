cbed244dc33f67afc36ca47e955a93b6bdba71d9
ADMIN\Admin <hoangsan0303@gmail.com>
2019-01-10 00:12:21 +0900

    commit migration and repository

---
+++ BanHangT.Data/Repository/ProductCategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using BanHangT.Data.Infrastructure;
+using BanHangT.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BanHangT.Data.Repository
+{
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
